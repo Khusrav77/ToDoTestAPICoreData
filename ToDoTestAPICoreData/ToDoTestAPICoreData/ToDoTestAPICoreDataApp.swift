0461711d11488707bf643a6bd1cdefc265935bab
Khusrav Safiev <147575346+Khusrav77@users.noreply.github.com>
2024-08-25 10:03:51 +0300

    Created  project in xcode

---
+++ ToDoTestAPICoreData/ToDoTestAPICoreData/ToDoTestAPICoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoTestAPICoreDataApp.swift
+//  ToDoTestAPICoreData
+//
+//  Created by Khusrav Safiev on 8/25/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoTestAPICoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
