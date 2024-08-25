05920147a4d355985a7839344700c4dabdc5df92
Khusrav Safiev <147575346+Khusrav77@users.noreply.github.com>
2024-08-25 12:10:33 +0300

    added CoreDataManeger and Model coredata

---
+++ ToDoTestAPICoreData/ToDoTestAPICoreData/CoreData/CoreDataManager.swift
@@ -0,0 +1,40 @@
+//
+//  CoreDataManager.swift
+//  ToDoTestAPICoreData
+//
+//  Created by Khusrav Safiev on 8/25/24.
+//
+
+import Foundation
+import CoreData
+
+
+final class CoreDataManager {
+    static let shared = CoreDataManager()
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "TodoDataModel")
+        container.loadPersistentStores { storeDescription, error in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        return container
+    }()
+    
+    var context: NSManagedObjectContext {
+        return persistentContainer.viewContext
+    }
+    
+    func saveContext() {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+}
