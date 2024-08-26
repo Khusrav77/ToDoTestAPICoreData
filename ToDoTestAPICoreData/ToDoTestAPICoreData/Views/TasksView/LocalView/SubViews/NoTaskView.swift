e1ae8c5864dd1515cee56c6962825d62b71af9f5
Khusrav Safiev <147575346+Khusrav77@users.noreply.github.com>
2024-08-26 22:38:41 +0300

    added NoTaskView

---
+++ ToDoTestAPICoreData/ToDoTestAPICoreData/Views/TasksView/LocalView/SubViews/NoTaskView.swift
@@ -0,0 +1,46 @@
+//
+//  NoTaskView.swift
+//  ToDoTestAPICoreData
+//
+//  Created by Khusrav Safiev on 8/26/24.
+//
+
+import SwiftUI
+
+struct NoTaskView: View {
+    var body: some View {
+        VStack{
+            
+            Spacer()
+            
+            // MARK: - Text
+            Text("THERE ARE NO TASKS\nWOULD YUO LIKE\n ADD?")
+                .font(.title)
+                .foregroundStyle(Color.tdPrimary)
+                .opacity(0.5)
+                .multilineTextAlignment(.center)
+            
+            Spacer()
+            
+            // MARK: - Button
+            NavigationLink(destination: AddTaskView()) {
+                ZStack{
+                    Circle()
+                        .frame(width: 100)
+                        .foregroundStyle(Color.tdPrimary)
+                        .opacity(0.5)
+                    
+                    Image(systemName: "plus")
+                        .font(.largeTitle)
+                        .foregroundStyle(Color.tdBackground2)
+                    
+                }
+            }
+            .padding(.bottom)
+        }
+    }
+}
+    #Preview {
+        NoTaskView()
+            .preferredColorScheme(.dark)
+    }
